0db77ec51895db6496a890729be91b4f2666c6ae
BOTAlex <zhentao2004@gmail.com>
2025-03-09 03:51:57 +0100

    LETS GOOO. PYTHON CONNECTED. finally

--- src-tauri/python/learningTauri/ZConnector.py
+++ src-tauri/python/learningTauri/ZConnector.py
@@ -3,26 +3,4 @@
 
 import cv2
 import os
-import win32com.client
-
-def get_list_of_system_cameras():
-    camera_list = []
-    
-    # Get the device manager object
-    wmi = win32com.client.Dispatch("WbemScripting.SWbemLocator")
-    services = wmi.ConnectServer(".", "root\\CIMV2")
-    devices = services.ExecQuery("Select * from Win32_PnPEntity where DeviceID like 'USB%'")
-
-    for device in devices:
-        if "video" in device.Description.lower():
-            # Extract camera name
-            camera_name = device.Description
-            # Check if the camera is available
-            for i in range(10):
-                cap = cv2.VideoCapture(i)
-                if cap.isOpened():
-                    camera_list.append((i, camera_name))
-                    cap.release()
-                    break
-    return camera_list
 
--- src-tauri/python/learningTauri/__init__.py
+++ src-tauri/python/learningTauri/__init__.py
@@ -1,17 +1,45 @@
-"""The tauri-app."""
+# --8<-- [start:command]
 
+import sys
+
+from anyio.from_thread import start_blocking_portal
+from pydantic import BaseModel
 from pytauri import (
     BuilderArgs,
+    Commands,
     builder_factory,
     context_factory,
 )
 
+commands: Commands = Commands()
+
+
+class Person(BaseModel):
+    name: str
+
+
+class Greeting(BaseModel):
+    message: str
+
+
+@commands.command()
+async def greet(body: Person) -> Greeting:
+    return Greeting(
+        message=f"Hello, {body.name}! You've been greeted from Python {sys.version}!"
+    )
+
+
+# --8<-- [end:command]
+
 
 def main() -> None:
-    """Run the tauri-app."""
-    app = builder_factory().build(
-        BuilderArgs(
-            context=context_factory(),
+    with start_blocking_portal("asyncio") as portal:  # or `trio`
+        app = builder_factory().build(
+            BuilderArgs(
+                context=context_factory(),
+                # 👇
+                invoke_handler=commands.generate_handler(portal),
+                # 👆
+            )
         )
-    )
-    app.run()
+        app.run()
