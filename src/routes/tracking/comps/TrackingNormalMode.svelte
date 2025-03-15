120ca569b729269490b81e6fc48e80f3577b947e
BOTAlex <zhentao2004@gmail.com>
2025-03-15 21:02:44 +0100

    Nice. overflow now supported. now able to handle more cameras

--- src/routes/tracking/comps/TrackingNormalMode.svelte
+++ src/routes/tracking/comps/TrackingNormalMode.svelte
@@ -8,27 +8,30 @@
     import { CameraStreamType } from "@src/lib/structs/CameraStreamType";
 </script>
 
-<div class="flex flex-col h-full w-full justify-center place-items-center gap-4">
-    <div class="flex justify-center w-full gap-4 mt-auto">
-        {#each Object.values(get(Cameras)) as camera}
-        {#if camera.isEnabled}
-        <CameraConnection streamType={CameraStreamType.Algorithmed} camera={camera}>
-            <div class="p-4">
-                
-                <div class="flex justify-between place-items-center">
-                    <div class="border-l border-primary pl-2 border-dashed">
-                        <div class="text-sm font-bold">{camera.position} camera</div>
-                        <div class="text-xs">Source type: {camera.sourceType}</div>
+<div class="flex flex-col h-full w-full">
+    <div class="overflow-x-auto px-4">
+        <div class="min-w-max mx-auto">
+            <div class="grid grid-flow-col auto-cols-min gap-4 p-4 justify-center">
+            {#each Object.values(get(Cameras)) as camera}
+            {#if camera.isEnabled}
+            <CameraConnection streamType={CameraStreamType.Algorithmed} camera={camera}>
+                <div class="p-4">
+                    
+                    <div class="flex justify-between place-items-center">
+                        <div class="border-l border-primary pl-2 border-dashed">
+                            <div class="text-sm font-bold">{camera.position} camera</div>
+                            <div class="text-xs">Source type: {camera.sourceType}</div>
+                        </div>
+                        <button class="btn btn-ghost btn-sm p-1"><ZoomIn/></button>
                     </div>
-                    <button class="btn btn-ghost btn-sm p-1"><ZoomIn/></button>
                 </div>
+            </CameraConnection>
+            {/if}
+            {/each}
             </div>
-        </CameraConnection>
-        {/if}
-        {/each}
+        </div>
     </div>
-    
-    <div class="mt-auto w-full h-64 border-t-4 border-base-300">
+    <div class="w-full min-h-64 border-t-4 border-base-300">
         <EtvrControls/>
     </div>
 </div>
