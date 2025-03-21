ddd019f33e2ddd7856708fe29ba48d997a072fc1
BOTAlex <zhentao2004@gmail.com>
2025-03-21 01:35:18 +0100

    Installed fs

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,16 +1,21 @@
-use serialport::{available_ports};
+use serialport::available_ports;
 
 #[tauri::command]
-fn get_list_of_serial() -> Vec<String>{
+fn get_list_of_serial() -> Vec<String> {
     match available_ports() {
         Ok(ports) => {
             if ports.is_empty() {
                 println!("No serial devices found.");
                 return Vec::new();
-            }else {
-                return ports.iter().filter(|&x| x.port_name.starts_with("COM") ||x.port_name.starts_with("/dev/tty")).map(|x| x.port_name.clone()).collect();
+            } else {
+                return ports
+                    .iter()
+                    .filter(|&x| {
+                        x.port_name.starts_with("COM") || x.port_name.starts_with("/dev/tty")
+                    })
+                    .map(|x| x.port_name.clone())
+                    .collect();
             }
-                
         }
         Err(_) => Vec::new(),
     }
@@ -25,6 +30,7 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
         .invoke_handler(tauri::generate_handler![get_list_of_serial])
